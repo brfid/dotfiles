9b3f4f63465a14e948cdc0bfeb38f9dc27a6b55a
brfid <brfid@icloud.com>
2026-03-08 20:01:11 -0400

    add outline.nvim, x11 keybinding (caps/ctrl swap)

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Auto-save on focus loss or leaving a buffer
+vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave" }, {
+  command = "silent! wa",
+})
